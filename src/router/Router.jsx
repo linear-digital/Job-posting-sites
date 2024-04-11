ef55319210182b8a2925db8f44ef334d69e9b882
linear-digital <tamizuddin.tr@gmail.com>
2024-04-11 22:19:38 +0600

    404: Not found page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import Home from '../pages/home/Home';
 import App from './../App';
 import Login from './../pages/Auth/Login';
 import Signup from '../pages/Auth/Signup';
+import Error from '../pages/Error/Error';
 
 const router = createBrowserRouter([
     {
@@ -22,9 +23,13 @@ const router = createBrowserRouter([
             {
                 path: 'signup',
                 element: <Signup />
+            },
+            {
+                path: '*',
+                element: <Error />
             }
         ]
-    }
+    },
 ])
 
 export default router;
