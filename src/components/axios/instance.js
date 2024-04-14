0f2c56f260c9d24c48dc3713ba7688f433841a18
linear-digital <tamizuddin.tr@gmail.com>
2024-04-15 04:07:40 +0600

    update instance.js

--- src/components/axios/instance.js
+++ src/components/axios/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookie  from 'js-cookie';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://job-planet-server.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
         'token': Cookie.get('accessToken')
